09cb4229a4277b8c3d165e5b6e348d157a375d6a
LauBeduneau <laurie.beduneau@gmail.com>
2023-09-23 15:02:55 +0200

    replace img by Image from next/image

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,7 @@
 import { LoginForm } from "@/components/forms";
 import type { Metadata } from "next";
 import Link from "next/link";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: 'Connexion',
@@ -12,11 +13,10 @@ export default function Login() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+          <Image
             className="mx-auto h-10 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+            alt="Your Company" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-blue">
             Connexion
           </h2>
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,5 +1,6 @@
 import { RegisterForm } from "@/components/forms";
 import type { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title: 'Création de compte',
@@ -13,11 +14,10 @@ export default function Register() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
+          <Image
             className="mx-auto h-10 w-auto"
             src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+            alt="Your Company" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Créer son compte
           </h2>
