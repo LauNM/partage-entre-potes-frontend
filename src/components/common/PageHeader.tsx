3125019180ebdb71c04a60812b6226092aab6b50
LauNM <laurie.beduneau@gmail.com>
2023-10-03 23:30:22 +0200

    wip page header

---
+++ src/components/common/PageHeader.tsx
@@ -0,0 +1,11 @@
+import Dropdown from "@/components/common/Dropdown";
+
+export default function PageHeader() {
+    return (
+        <div>
+            <input />
+            <button>Mes réservations</button>
+            <Dropdown />
+        </div>
+    )
+}
