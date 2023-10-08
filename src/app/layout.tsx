7b677f34d974249cc7ced6f07e7e1add510c490d
LauNM <laurie.beduneau@gmail.com>
2023-10-08 20:38:41 +0200

    add app directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,6 @@ export default function RootLayout({
       <body className={inter.className}>
         <Provider>
           <Setup />
-          <Navbar />
           {children}
         </Provider>
       </body>
