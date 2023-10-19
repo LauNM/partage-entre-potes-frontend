d1f9ba001622997b7b484fa0b5ca3c08627b07f7
LauNM <laurie.beduneau@gmail.com>
2023-10-19 23:32:20 +0200

    fetch profile summary

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,12 +1,58 @@
 'use client';
 
+import {useEffect, useState} from "react";
+import {store} from "@/redux/store";
+import axios from "axios";
+import {InfoCard} from "@/components/common";
+
+const token = store.getState().auth.userToken;
+
+async function fetchSummary() {
+    const response = await axios
+        .get(`${process.env.NEXT_PUBLIC_HOST}/api/profile/summary/`, {
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            }
+        })
+    return response.data
+}
+
 export default function Profile() {
-   return (
+    const [data, setData] = useState(null)
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        fetchSummary()
+            .then((data) => {
+                setData(data)
+                setLoading(false)
+            })
+    }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!data) return <p>No profile data</p>
+
+    return (
         <>
             <p>Profil</p>
-            <ul>
+            <div className="grid grid-cols-2 gap-4 py-2 text-center">
+                <InfoCard type={'blue'}>
+                    <p>{data.total_friends}</p>
+                    <p>amis</p>
+                </InfoCard>
+                <InfoCard type={'green'}>
+                    <p>{data.total_products}</p>
+                    <p>produits</p>
+                </InfoCard>
+            </div>
 
-            </ul>
+            <div className="grid grid-cols-1 text-center">
+                <InfoCard type={'red'}>
+                    <p>{data.total_friends_products}</p>
+                    <p>Produits à emprunter</p>
+                </InfoCard>
+            </div>
         </>
     )
 }
